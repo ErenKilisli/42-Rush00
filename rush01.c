3d1323fc98a6b1beb029ae658d7958f07e293858
ErenKilisli <ierenkilisli@gmail.com>
2024-10-10 10:59:53 +0300

    commititti

---
+++ rush01.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pgawthro <pgawthro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 12:22:01 by pgawthro          #+#    #+#             */
+/*   Updated: 2024/08/04 11:30:33 by pgawthro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	first_row(int x, int column)
+{
+	if (column == 0)
+	{
+		ft_putchar('/');
+	}
+	else if (column == x - 1)
+	{
+		ft_putchar('\\');
+	}
+	else
+	{
+		ft_putchar('*');
+	}
+}
+
+void	last_row(int x, int column)
+{
+	if (column == 0)
+	{
+		ft_putchar('\\');
+	}
+	else if (column == x - 1)
+	{
+		ft_putchar('/');
+	}
+	else
+	{
+		ft_putchar('*');
+	}
+}
+
+void	other_rows(int x, int column)
+{
+	if (column == 0)
+	{
+		ft_putchar('*');
+	}
+	else if (column == x - 1)
+	{
+		ft_putchar('*');
+	}
+	else
+	{
+		ft_putchar(' ');
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	row;
+	int	column;
+
+	row = 0;
+	while (row < y)
+	{
+		column = 0;
+		while (column < x)
+		{
+			if (row == 0)
+				first_row(x, column);
+			else if (row == y - 1)
+				last_row(x, column);
+			else
+				other_rows(x, column);
+			column++;
+		}
+		row++;
+		ft_putchar('\n');
+	}
+}
---
+++ rush04.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pgawthro <pgawthro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 15:42:35 by pgawthro          #+#    #+#             */
+/*   Updated: 2024/08/04 11:33:58 by pgawthro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void    first_row(int x, int column)
+{
+    if (column == 0)
+    {
+        ft_putchar('A');
+    }
+    else if (column == x - 1)
+    {
+        ft_putchar('C');
+    }
+    else
+    {
+        ft_putchar('B');
+    }
+}
+
+void    last_row(int x, int column)
+{
+    if (column == 0)
+    {
+        ft_putchar('C');
+    }
+    else if (column == x - 1)
+    {
+        ft_putchar('A');
+    }
+    else
+    {
+        ft_putchar('B');
+    }
+}
+
+void    other_rows(int x, int column)
+{
+    if (column == 0)
+    {
+        ft_putchar('B');
+    }
+    else if (column == x - 1)
+    {
+        ft_putchar('B');
+    }
+    else
+    {
+        ft_putchar(' ');
+    }
+}
+
+void    rush(int x, int y)
+{
+    int row;
+    int column;
+
+    row = 0;
+    while (row < y)
+    {
+        column = 0;
+        while (column < x)
+        {
+            if (row == 0)
+                first_row(x, column);
+            else if (row == y - 1)
+                last_row(x, column);
+            else
+                other_rows(x, column);
+            column++;
+        }
+        row++;
+        ft_putchar('\n');
+    }
+}	
